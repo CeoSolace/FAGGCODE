2c16b3e4acf7973f6d18eab35494ab6d39bdaa32
Solace <monkdev9@gmail.com>
2026-02-01 15:11:41 +0000

    Create Root.js

---
+++ remotion/Root.js
@@ -0,0 +1,16 @@
+import { Composition } from 'remotion';
+import { IntroVideo } from './IntroVideo';
+export const RemotionRoot = () => {
+  return (
+    <>
+      <Composition
+        id="FreeAgentsIntro"
+        component={IntroVideo}
+        durationInFrames={900}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+    </>
+  );
+};
