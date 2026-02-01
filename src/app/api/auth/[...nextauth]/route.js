d80f8ce429916eaa5d814bde8894d0abd7cebf21
Solace <monkdev9@gmail.com>
2026-02-01 15:09:12 +0000

    Create route.js

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,27 @@
+import NextAuth from 'next-auth';
+import DiscordProvider from 'next-auth/providers/discord';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
+import clientPromise from '../../../lib/mongodb';
+export const authOptions = {
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+  adapter: MongoDBAdapter(clientPromise),
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id;
+      session.user.role = user.role;
+      session.user.games = user.games;
+      return session;
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
